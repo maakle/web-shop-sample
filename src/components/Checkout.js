4dca750a8df01656b2c7b1ba8ae3e3dcf435a773
mathiasklenk <mathias.j.klenk@gmail.com>
2020-07-08 03:01:31 +0000

    Added the passbase button and syled components dependency

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import VerifyButton from "@passbase/button/react";
 
 const Checkout = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -25,7 +26,22 @@ const Checkout = () => {
           <h6>Verification Necessary</h6>
         </div>
         <div className="modal-body">
-          <p>Test</p>
+          <VerifyButton
+        apiKey={"YOUR_PUBLISHABLE_API_KEY"}
+        onFinished={(error, authKey, additionalAttributes) => {}}
+        integrationType={"signup"}
+        additionalAttributes={{
+          customer_user_id: "SOME_USER_ID"
+        }}
+        prefillAttributes={{
+          email: "",
+          country: "en"
+        }}
+        theme={{
+          accentColor: "",
+          font: "Arial"
+        }}
+      />
         </div>
         <div className="modal-footer" />
       </div>
