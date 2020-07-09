525d6984a93b22446ce4676bb09ac97cb4c3c5a4
Mathias Klenk <mathiasklenk@gmx.de>
2020-07-09 14:24:47 -0400

    Implemented better Modal description

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -23,26 +23,30 @@ const Checkout = () => {
 
       <div className={containerClass}>
         <div className="modal-header">
-          <h6>Verification Necessary</h6>
+          <h5 className="modal-title">Verification Necessary</h5>
         </div>
         <div className="modal-body">
-          <VerifyButton
-            apiKey=
-            {"ebb8c2f2c2b27d10a1dfdf1bd0a882455b0f5cfcfd37f2a9020d24cfbe08feaa"}
-            onFinished={(error, authKey, additionalAttributes) => {}}
-            integrationType={"signup"}
-            additionalAttributes={{
-              customer_user_id: "SOME_USER_ID"
-            }}
-            prefillAttributes={{
-              email: "",
-              country: "en"
-            }}
-            theme={{
-              accentColor: "",
-              font: "Arial"
-            }}
-          />
+          <p>In order to finish the purchase you need to verify your age. Please complete the verification flow my pressing the button below.
+          </p>
+          <div className="pb-button-wrapper">
+            <VerifyButton
+              apiKey=
+              {"ebb8c2f2c2b27d10a1dfdf1bd0a882455b0f5cfcfd37f2a9020d24cfbe08feaa"}
+              onFinished={(error, authKey, additionalAttributes) => {}}
+              integrationType={"signup"}
+              additionalAttributes={{
+                customer_user_id: ""
+              }}
+              prefillAttributes={{
+                email: "",
+                country: ""
+              }}
+              theme={{
+                accentColor: "",
+                font: ""
+              }}
+            />
+          </div>
         </div>
         <div className="modal-footer" />
       </div>
