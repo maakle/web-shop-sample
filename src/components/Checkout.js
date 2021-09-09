55d8163aea5df57fa6420243ab69cc75d2e7bc25
Mathias Klenk <mathiasklenk@gmx.de>
2021-09-09 15:10:06 -0400

    Updated passbase

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -26,25 +26,19 @@ const Checkout = () => {
           <h5 className="modal-title">Age verification required</h5>
         </div>
         <div className="modal-body">
-          <p>You're probably old enough to place this order but in order to finish the purchase you need to verify your age. Please do so by clicking this button.
+          <p>
+            You're probably old enough to place this order but in order to
+            finish the purchase you need to verify your age. Please do so by
+            clicking this button.
           </p>
           <div className="pb-button-wrapper">
             <VerifyButton
-              apiKey=
-              {"2b4598cdbb809d8b7a34ba534d76447634ed9a8d58bb498421f72cd8187df048"}
-              onFinished={(error, authKey, additionalAttributes) => {}}
-              integrationType={"signup"}
-              additionalAttributes={{
-                customer_user_id: ""
-              }}
-              prefillAttributes={{
-                email: "",
-                country: ""
-              }}
-              theme={{
-                accentColor: "",
-                font: ""
-              }}
+              apiKey={
+                "2b4598cdbb809d8b7a34ba534d76447634ed9a8d58bb498421f72cd8187df048"
+              }
+              onFinish={(identityAccessKey) => {}}
+              onError={(errorCode) => {}}
+              onStart={() => {}}
             />
           </div>
         </div>
